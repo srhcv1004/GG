bc5c23bf2eda0c38136d6a608c115f36fa1f37ba
dodam890 <dodam260@gmail.com>
2018-01-23 12:59:03 +0900

    1
    1

--- D3D_Framework/D3D_Framework/D3D_Framework/cEquip.cpp
+++ D3D_Framework/D3D_Framework/D3D_Framework/cEquip.cpp
@@ -9,6 +9,7 @@ cEquip::cEquip()
 	, m_eChangePartKind(E_PARTS_BODY)
 	, m_arChangePartsNum()
 {
+	D3DXMatrixIdentity(&m_matXHeadLocalTM);
 	D3DXMatrixIdentity(&m_matXHeadWorldTM);
 }
 
@@ -19,7 +20,15 @@ cEquip::~cEquip()
 void cEquip::Setup(D3DXMATRIXA16* pXPlayerWorldTM)
 {
 	m_pXPlayerWorldTM = pXPlayerWorldTM;
+
+	m_arChangePartsNum[E_PARTS_HEAD] = 0;
+	m_arChangePartsNum[E_PARTS_BODY] = 17;
+	m_arChangePartsNum[E_PARTS_HAND] = 17;
+	m_arChangePartsNum[E_PARTS_LEG] = 17;
+	m_arChangePartsNum[E_PARTS_HAIR] = 0;
+
 	this->SetupParts();
+	//this->SetupMaterial();
 }
 
 void cEquip::Release()
@@ -44,11 +53,17 @@ void cEquip::Update()
 
 void cEquip::Render()
 {
+<<<<<<< HEAD
 	D_DEVICE->SetRenderState(D3DRS_LIGHTING, FALSE);
+=======
+>>>>>>> 0f2ac7118d51c54005fc3acb7dc25d6823ddcf35
 	this->RenderChangeKind();
+
+	D_DEVICE->SetRenderState(D3DRS_LIGHTING, FALSE);
 
 	for (size_t i = 0; i < m_vecSkinnedPlayer.size(); i++)
 	{
+<<<<<<< HEAD
 		if (i == 0)
 		{
 			ST_BONE* pBone = (ST_BONE*)m_vecSkinnedPlayer[E_PARTS_BODY]->GetRootBone();
@@ -64,26 +79,36 @@ void cEquip::Render()
 		{
 			ST_BONE* pBone = (ST_BONE*)m_vecSkinnedPlayer[E_PARTS_BODY]->GetRootBone();
 			ST_BONE* pFindBone = (ST_BONE*)D3DXFrameFind(pBone, "Bip01-Neck");
+=======
+		if (!m_vecSkinnedPlayer[i]) continue;
+>>>>>>> 0f2ac7118d51c54005fc3acb7dc25d6823ddcf35
 
-			D3DXMATRIXA16 matWorld, matT;
-			D3DXMatrixTranslation(&matT, 3.F, 0.F, 0.F);
-			matWorld = matT * pFindBone->matWorldTM;
+		ST_BONE* pBoneBody = (ST_BONE*)m_vecSkinnedPlayer[E_PARTS_BODY]->GetRootBone();
+		ST_BONE* pFindBoneBody = (ST_BONE*)D3DXFrameFind(pBoneBody, "Bip01-Neck");
 
-			D_DEVICE->SetTransform(D3DTS_WORLD, &matWorld);
+		D3DXMATRIXA16 matWorld;
+		D3DXMatrixIdentity(&matWorld);
 
-			if (m_vecSkinnedPlayer[i])
-				m_vecSkinnedPlayer[i]->Render();
-		}
-		else
+		switch (i)
 		{
-			D3DXMATRIXA16 matWorld;
-			D3DXMatrixIdentity(&matWorld);
+			case E_PARTS_HEAD:
+			{
+				matWorld = pFindBoneBody->matWorldTM;
+			}
+			break;
 
-			D_DEVICE->SetTransform(D3DTS_WORLD, &matWorld);
+			case E_PARTS_HAIR:
+			{
+				ST_BONE* pBoneHead = (ST_BONE*)m_vecSkinnedPlayer[E_PARTS_HEAD]->GetRootBone();
+				ST_BONE* pFindBoneHead = (ST_BONE*)D3DXFrameFind(pBoneHead, "Dummy_Face");
 
-			if (m_vecSkinnedPlayer[i])
-				m_vecSkinnedPlayer[i]->Render();
+				matWorld = pFindBoneHead->matWorldTM * pFindBoneBody->matWorldTM;
+			}
+			break;
 		}
+
+		D_DEVICE->SetTransform(D3DTS_WORLD, &matWorld);
+		m_vecSkinnedPlayer[i]->Render();
 	}
 }
 
@@ -91,19 +116,19 @@ void cEquip::SetupParts()
 {
 	m_vecSkinnedPlayer.resize(E_PARTS_END);
 
-	// Head parts
-	cSkinnedMesh* pHead = new cSkinnedMesh();
-	pHead->Setup("XFile/XItem", "[0]Head.x");
-	m_vecSkinnedPlayer[E_PARTS_HEAD] = pHead;
-	m_vecSkinnedPlayer[E_PARTS_HEAD]->SetMatWorldPtr(&m_matXHeadWorldTM);
+	//// Head parts
+	cXItem* pXHead =
+		(cXItem*)(D_ITEMMANAGER->FindItem(m_arChangePartsNum[E_PARTS_HEAD], cItemManager::E_PARTS_HEAD));
+	m_vecSkinnedPlayer[E_PARTS_HEAD] = pXHead->GetXItem();
+	m_vecSkinnedPlayer[E_PARTS_HEAD]->SetMatWorldPtr(NULL);
 
-	// Head parts
-	cSkinnedMesh* pHair = new cSkinnedMesh();
-	pHair->Setup("XFile/XItem", "[0]Hair.x");
-	m_vecSkinnedPlayer[E_PARTS_HAIR] = pHair;
-	m_vecSkinnedPlayer[E_PARTS_HAIR]->SetMatWorldPtr(&m_matXHeadWorldTM);
+	//// Head parts
+	cXItem* pXHair =
+		(cXItem*)(D_ITEMMANAGER->FindItem(m_arChangePartsNum[E_PARTS_HAIR], cItemManager::E_PARTS_HAIR));
+	m_vecSkinnedPlayer[E_PARTS_HAIR] = pXHair->GetXItem();
+	m_vecSkinnedPlayer[E_PARTS_HAIR]->SetMatWorldPtr(NULL);
 
-	// Body parts
+	//Body parts
 	cXItem* pXBody = 
  		(cXItem*)(D_ITEMMANAGER->FindItem(m_arChangePartsNum[E_PARTS_BODY], cItemManager::E_PARTS_BODY));
 	m_vecSkinnedPlayer[E_PARTS_BODY] = pXBody->GetXItem();
@@ -120,6 +145,14 @@ void cEquip::SetupParts()
 		(cXItem*)(D_ITEMMANAGER->FindItem(m_arChangePartsNum[E_PARTS_LEG], cItemManager::E_PARTS_LEG));
 	m_vecSkinnedPlayer[E_PARTS_LEG] = pXLeg->GetXItem();
 	m_vecSkinnedPlayer[E_PARTS_LEG]->SetMatWorldPtr(m_pXPlayerWorldTM);
+}
+
+void cEquip::SetupMaterial()
+{
+	ZeroMemory(&m_sD3DMaterial, sizeof(D3DMATERIAL9));
+	m_sD3DMaterial.Diffuse = D3DXCOLOR(1.F, 1.F, 1.F, 1.F);
+	m_sD3DMaterial.Ambient = D3DXCOLOR(1.F, 1.F, 1.F, 1.F);
+	m_sD3DMaterial.Specular = D3DXCOLOR(1.F, 1.F, 1.F, 1.F);
 }
 
 void cEquip::RenderChangeKind()
@@ -145,11 +178,30 @@ void cEquip::RenderChangeKind()
 			strncpy_s(str, 512, "[Leg 체인지]", 512);
 		}
 		break;
+
+		case cEquip::E_PARTS_HAIR:
+		{
+			strncpy_s(str, 512, "[Hair 체인지]", 512);
+		}
+		break;
+
+		case cEquip::E_PARTS_HEAD:
+		{
+			strncpy_s(str, 512, "[Head 체인지]", 512);
+		}
+		break;
 	}
 
 	D_FONTMANAGER->DrawFontText("TimerFont", str,
 		NULL,
 		RectMake(D_WINSIZEX / 2, D_WINSIZEY / 2, 0, 0),
+		D3DCOLOR_XRGB(255, 255, 255));
+
+	sprintf_s(str, "[%d번 아이템]", m_arChangePartsNum[m_eChangePartKind]);
+	
+	D_FONTMANAGER->DrawFontText("TimerFont", str,
+		NULL,
+		RectMake(D_WINSIZEX / 2, D_WINSIZEY / 2 + 20, 0, 0),
 		D3DCOLOR_XRGB(255, 255, 255));
 }
 
@@ -170,16 +222,45 @@ void cEquip::ChangeParts()
 		m_eChangePartKind = E_PARTS_LEG;
 	}
 	
+	if (D_KEYMANAGER->IsOnceKeyDown(VK_F4))
+	{
+		m_eChangePartKind = E_PARTS_HAIR;
+	}
+
+	if (D_KEYMANAGER->IsOnceKeyDown(VK_F5))
+	{
+		m_eChangePartKind = E_PARTS_HEAD;
+	}
 
 	if (D_KEYMANAGER->IsOnceKeyDown(VK_SPACE))
 	{
-		if (m_arChangePartsNum[m_eChangePartKind] == 0) m_arChangePartsNum[m_eChangePartKind] = 1;
-		else if (m_arChangePartsNum[m_eChangePartKind] == 1) m_arChangePartsNum[m_eChangePartKind] = 0;
+
+		size_t sizeMap = D_ITEMMANAGER->GetMapXItem(cItemManager::E_PARTS(m_eChangePartKind));
+
+		if (sizeMap - 1 > m_arChangePartsNum[m_eChangePartKind])
+			m_arChangePartsNum[m_eChangePartKind] += 1;
+		else
+			m_arChangePartsNum[m_eChangePartKind] = 0;
 
 		cXItem* pXParts =
 			(cXItem*)(D_ITEMMANAGER->FindItem(m_arChangePartsNum[m_eChangePartKind], cItemManager::E_PARTS(m_eChangePartKind)));
 		m_vecSkinnedPlayer[m_eChangePartKind] = pXParts->GetXItem();
-		m_vecSkinnedPlayer[m_eChangePartKind]->SetMatWorldPtr(m_pXPlayerWorldTM);
+
+		switch (m_eChangePartKind)
+		{
+			case cEquip::E_PARTS_HEAD:
+			case cEquip::E_PARTS_HAIR:
+			{
+				m_vecSkinnedPlayer[m_eChangePartKind]->SetMatWorldPtr(NULL);
+			}
+			break;
+
+			default:
+			{
+				m_vecSkinnedPlayer[m_eChangePartKind]->SetMatWorldPtr(m_pXPlayerWorldTM);
+			}
+			break;
+		}
 
 		m_pXPlayerAnimation->AnimationChangeByState();
 	}
--- D3D_Framework/D3D_Framework/D3D_Framework/cMapCastle.cpp
+++ D3D_Framework/D3D_Framework/D3D_Framework/cMapCastle.cpp
@@ -4,9 +4,11 @@
 #include "cGrid.h"
 #include "cSprite.h"
 #include "cXPlayer.h"
+#include "cSkinnedMesh.h"
 
 cMapCastle::cMapCastle()
 	: m_pLineGrid(NULL)
+	, m_pWeaponTest(NULL)
 {
 }
 
@@ -21,16 +23,25 @@ void cMapCastle::Setup()
 	m_pLineGrid = new cLineGrid();
 	m_pLineGrid->Setup();
 
-	m_pPlayer = new cXPlayer();
-	m_pPlayer->Setup();
+	//m_pPlayer = new cXPlayer();
+	//m_pPlayer->Setup();
 
 	m_pCamera = new cGameCamera();
-	m_pCamera->Setup(&m_pPlayer->GetPosition());
+	m_pCamera->Setup();
+	//&m_pPlayer->GetPosition()
+
+	m_pWeaponTest = new cSkinnedMesh();
+	m_pWeaponTest->Setup("XFile/XItem/Weapon", "[0]Weapon.X");
 }
 
 void cMapCastle::Release()
 {
 	cMap::Release();
+<<<<<<< HEAD
+=======
+
+	D_SAFE_DELETE(m_pCamera);
+>>>>>>> 0f2ac7118d51c54005fc3acb7dc25d6823ddcf35
 
 	D_SAFE_RELEASE(m_pLineGrid);
 	D_SAFE_DELETE(m_pLineGrid);
@@ -46,8 +57,11 @@ void cMapCastle::Update()
 	if (m_pLineGrid)
 		m_pLineGrid->Update();
 
-	if (m_pPlayer)
-		m_pPlayer->Update();
+	//if (m_pPlayer)
+	//	m_pPlayer->Update();
+
+	if (m_pWeaponTest)
+		m_pWeaponTest->Update();
 
 	this->ChangeWorldMapScene();
 }
@@ -59,8 +73,11 @@ void cMapCastle::Render()
 	if (m_pLineGrid)
 		m_pLineGrid->Render();
 
-	if (m_pPlayer)
-		m_pPlayer->Render();
+	//if (m_pPlayer)
+	//	m_pPlayer->Render();
+
+	if (m_pWeaponTest)
+		m_pWeaponTest->Render();
 
 	this->RenderNowMapInfo();
 }
